3e5c98cb2922de37ae3399039971a39eef171c63
George <george.riabov3105@gmail.com>
2025-10-25 20:39:28 +0200

    finish task 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const spanValue = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  spanValue.textContent = randomColor;
+});
